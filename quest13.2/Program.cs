e5e9a674c8ab5104bdc872f2a60c3cf6ad25b0bd
Vladislav Il'in <vladi5lav.ilin@yandex.ru>
2023-10-19 21:01:26 +0300

    Добавьте файлы проекта.

---
+++ quest13.2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quest13._2
+{
+
+    abstract class AbstractHandler
+    {
+        public abstract void Create();
+        public abstract void Open();
+        public abstract void Change();
+        public abstract void Save();
+    }
+
+    class XMLHandler : AbstractHandler
+    {
+        public override void Open() { Console.WriteLine("Открыт XML документ"); }
+        public override void Create() { Console.WriteLine("Создан XML документ"); }
+        public override void Change() { Console.WriteLine("Изменен XML документ"); }
+        public override void Save() { Console.WriteLine("Сохранен XML документ"); }
+    }
+
+    class TXTHandler : AbstractHandler
+    {
+        public override void Open() { Console.WriteLine("Открыт TXT документ"); }
+        public override void Create() { Console.WriteLine("Создан TXT документ"); }
+        public override void Change() { Console.WriteLine("Изменен TXT документ"); }
+        public override void Save() { Console.WriteLine("Сохранен TXT документ"); }
+    }
+
+    class DOCHandler : AbstractHandler
+    {
+        public override void Open() { Console.WriteLine("Открыт DOC документ"); }
+        public override void Create() { Console.WriteLine("Создан DOC документ"); }
+        public override void Change() { Console.WriteLine("Изменен DOC документ"); }
+        public override void Save() { Console.WriteLine("Сохранен DOC документ"); }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите формат документа (XML/TXT/DOC):");
+            string format = Console.ReadLine();
+
+            AbstractHandler handler;
+
+            switch (format.ToUpper())
+            {
+                case "XML": handler = new XMLHandler(); break;
+                case "TXT": handler = new TXTHandler(); break;
+                case "DOC": handler = new DOCHandler(); break;
+                default: Console.WriteLine("Неподдерживаемый формат документа"); return;
+            }
+
+            handler.Create();
+            handler.Open();
+            handler.Change();
+            handler.Save();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ quest13.3/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quest13._3
+{
+    interface IPlayable { void Play(); void Pause(); void Stop(); }
+
+    interface IRecodable { void Record(); void Pause(); void Stop(); }
+
+    class Player : IPlayable, IRecodable
+    {
+        public void Play() { Console.WriteLine("Проигрывание\n"); }
+        public void Record() { Console.WriteLine("Запись\n"); }
+        public void Pause() { Console.WriteLine("Пауза\n"); }
+        public void Stop() { Console.WriteLine("Остановка\n"); }
+    }
+
+    class Program
+    {
+        public static string PrintFirstChoise()
+        {
+            Console.WriteLine("Выберите действие:\n1. Проигрывание\n2. Запись\n3. Выход\n");
+            return null;
+        }
+        public static string PrintPlayerChoise()
+        {
+            Console.WriteLine("Выберите действие:\n1. Play\n2. Pause\n3. Stop\n");
+            return null;
+        }
+        public static string PrintRecorderChoise()
+        {
+            Console.WriteLine("Выберите действие:\n1. Record\n2. Pause\n3. Stop\n");
+            return null;
+        }
+
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+
+            PrintFirstChoise();
+
+            string firstCoise;
+            string secondCoise;
+            while (int.Parse(firstCoise = Console.ReadLine()) != 3)
+            {
+                switch (firstCoise)
+                {
+                    case "1":  /// Проигрываине
+                        PrintPlayerChoise();
+                        while (int.Parse(secondCoise = Console.ReadLine()) != 3)
+                        {
+                            switch (secondCoise)
+                            {
+                                case "1": player.Play();break; // Проигрывание
+                                case "2": player.Pause(); break; // Пауза
+                            }
+                            PrintPlayerChoise();
+                        }
+                        player.Stop(); break; // Остановка
+
+                    case "2":  /// Запись
+                        PrintRecorderChoise();
+
+                        while (int.Parse(secondCoise = Console.ReadLine()) != 3)
+                        {
+                            switch (secondCoise)
+                            {
+                                case "1": player.Record(); break; // Проигрывание
+                                case "2": player.Pause(); break; // Пауза
+                            }
+                            PrintRecorderChoise();
+                        }
+                        player.Stop(); break; // Остановка
+
+                    default: Console.WriteLine("Ошибка, введите 1 или 2\n"); break;
+                }
+                PrintFirstChoise();
+            }
+        }
+    }
+}
